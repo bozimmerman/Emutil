9f62ad6f2b9edef52d5896221fc7f4991e54bf52
bozimmerman <bo@zimmers.net>
2006-10-03 05:11:13 +0000

    git-svn-id: svn://192.168.1.10/public/Emutil@11493 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/emutil/D64Search.java
+++ com/planet_ink/emutil/D64Search.java
@@ -1,11 +1,25 @@
+package com.planet_ink.emutil;
 import java.io.*;
 import java.security.MessageDigest;
 import java.util.*;
-public class D64Search 
+/* 
+Copyright 2000-2006 Bo Zimmerman
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/public class D64Search 
 {
 	// oliver netlib.64
 	// todo: add file masks to options
-	// www.paradroid.net/diskimage
 	// usb byteblaster mv
 	// altera free developers kit usbblaster
 	public static final String D64IMAGE=".D64";
