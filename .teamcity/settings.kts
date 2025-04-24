2b7df167ba9c28029209c0f5b78b08a374cb2223
bozimmerman <bo@zimmers.net>
2025-04-24 22:13:44 +0000

    git-svn-id: svn://192.168.1.10/public/Emutil@23622 0d6f1817-ed0e-0410-87c9-987e46238f29

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,6 +2,7 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.ant
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 /*
 The settings script is an entry point for defining a TeamCity
